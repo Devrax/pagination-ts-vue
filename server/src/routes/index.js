3976516bed82381cafcfbd1b108db27266da7d70
Rafael Mejia <devrafx@gmail.com>
2019-10-20 14:15:21 -0400

    add: routes 'create' for create data and store into mongodb and 'Article' use for watch the data into de database

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,12 +1,26 @@
-const { Router } = require('express');
-        router = Router();
+const { Router } = require('express'),
+        faker = require('faker'),
+        router = Router(),
+        Article = require('../models/articles.model');
 
-router.get('/create', (req, res) => {
-    res.send('Received');
-})
+router.get('/create', async (req, res) => {
+    
+        for(let i = 0; i < 100; i++){
+            await Article.create({
+                id: faker.random.uuid(),
+                title: faker.name.title(),
+                description: faker.lorem.paragraph(),
+                imgURL: faker.image.imageUrl()
+            })
+        }
 
-router.get('/articles', (req, res) => {
+        res.send('100 records were created');
+    })
 
-})
+    .get('/articles', async (req, res) => {
+        const articles = await Article.find();
+
+        res.json(articles);
+    })
 
 module.exports = router;
